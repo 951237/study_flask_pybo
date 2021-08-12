661e9f6c0ed5c5ef0cfaafa5a8be6ae965c79efd
951237 <951237@gmail.com>
2021-08-13 01:44:48 +0900

    Doc : Update - 라우터 구현하면서 '/'누락된 오류 수정

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -14,7 +14,7 @@ def index():
     question_list = Question.query.order_by(Question.create_date.desc())  # 역순정렬
     return render_template('question/question_list.html', question_list=question_list)
 
-@bp.route('/detail/<int:question_id>')
+@bp.route('/detail/<int:question_id>/')
 def detail(question_id):
     question = Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html', question=question)
