920a85504104a4672c1304ad1b1b0f16734858d0
951237 <951237@gmail.com>
2021-08-12 23:47:35 +0900

    Doc : Update - 오탈자 수정 integer, URI

--- pybo/models.py
+++ pybo/models.py
@@ -9,8 +9,8 @@ class Question(db.Model):
     create_date = db.Column(db.DateTime(), nullable = False)    # 날짜 시간 속성
 
 class Answer(db.Model):
-    id = db.Column(db.Interger, primary_key = True)
-    question_id = db.Column(db.Interger, db.ForeignKey('question.id', ondelete = 'CASCADE'))    # foreignkey - 외부키 , ondelete - 삭제연동 cascade - 질문삭제시 답변도 함께 삭제
-    question = db.Relationship('Question', backref=db.backref('answer_set'))    # 답변 모델에서 질문 모델을 참조하기 위해생성. relationship 참조, backref 역참조
+    id = db.Column(db.Integer, primary_key = True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete = 'CASCADE'))    # foreignkey - 외부키 , ondelete - 삭제연동 cascade - 질문삭제시 답변도 함께 삭제
+    question = db.relationship('Question', backref=db.backref('answer_set'))    # 답변 모델에서 질문 모델을 참조하기 위해생성. relationship 참조, backref 역참조
     content = db.Column(db.Text(), nullable = False)
     create_date = db.Column(db.DateTime(), nullable = False)
