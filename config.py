d73a552c86aeb846a9d1d8f2aa77af0ff4ba7b09
951237 <951237@gmail.com>
2021-08-12 23:02:20 +0900

    Doc : Update - 주석추가

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URL = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))  # 데이터베이스 접속주소
+SQLALCHEMY_TRACK_MODIFICATIONS = False # sqlalchemy 이벤트 처리 옵션
 
